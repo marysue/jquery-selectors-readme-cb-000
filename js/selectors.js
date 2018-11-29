594082c3ad1f2987031f805a938ec3fa115fd235
Mary Lark <mary@marylark.com>
2018-11-29 22:18:03 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem () {
+  var tmp = $("#pic-list:first-child");
+  return tmp;
+  //return tmp[0].firstElementChild.innerHTML;
+}
